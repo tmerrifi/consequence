c6179106b55eb0e9ed9c0ef9773faaffa04fd56a
tmerrifi <tmerri4@uic.edu>
2016-02-01 13:50:17 -0600

    Made some simple changes, beefed up the test, etc....

--- tests/fgl_simple.c
+++ tests/fgl_simple.c
@@ -6,11 +6,12 @@
 #include <sched.h>
 
 #define PAGE_SIZE 4096
-#define NUM_OF_PAGES 10
-#define ARR_SIZE_IN_BYTES (PAGE_SIZE * NUM_OF_PAGES)
+#define NUM_OF_PAGES 1
+
+#define ARR_SIZE_IN_BYTES (16)
 
 #ifndef LOCK_GRANULARITY_IN_BYTES
-#define LOCK_GRANULARITY_IN_BYTES (PAGE_SIZE*1)
+#define LOCK_GRANULARITY_IN_BYTES (8)
 #endif
 
 #define NUM_MUTEXES (ARR_SIZE_IN_BYTES/LOCK_GRANULARITY_IN_BYTES)
@@ -25,10 +26,9 @@
 #define CS_WORK 1
 #endif
 
-#define OUTER_ITERATIONS ((1<<7)/NUM_THREADS)
-#define INNER_ITERATIONS ((1<<7)/NUM_THREADS)
+#define ITERATIONS 1024
 
-uint32_t arr[ARR_SIZE_IN_BYTES];
+uint32_t arr[NUM_OBJECTS];
 
 pthread_mutex_t mutexes[NUM_MUTEXES];
 
@@ -53,20 +53,15 @@ int sum_array(){
 }
 
 void * do_work(void * _id){
-    printf("%d\n", sched_getcpu());
+    int i;
     int id =* ((int *)_id);
-    for (int i=0;i<OUTER_ITERATIONS;++i){
-        if (i % 10==0){
-            printf("thread: %d at iteration %d\n", id, i);
-        }
-        for (int j=0;j<INNER_ITERATIONS;++j){
-            int random_index = rand() % NUM_OBJECTS;
-            int lock_index = get_lock_index(random_index);
-            pthread_mutex_lock(&mutexes[lock_index]);
-            arr[random_index]++;
-            pause_thread(CS_WORK);
-            pthread_mutex_unlock(&mutexes[lock_index]);
-        }
+    for (i=0;i<ITERATIONS;++i){
+        int random_index = rand() % NUM_OBJECTS;
+        int lock_index = get_lock_index(random_index);
+        pthread_mutex_lock(&mutexes[lock_index]);
+        arr[random_index]++;
+        pause_thread(CS_WORK);
+        pthread_mutex_unlock(&mutexes[lock_index]);
     }
     return NULL;
 }
@@ -80,7 +75,7 @@ int main(){
 
     //initialize array
     memset((uint8_t *)arr, 0, ARR_SIZE_IN_BYTES); 
-    
+
     int ids[NUM_THREADS];
     pthread_t threads[NUM_THREADS];
     for (int i=0;i<NUM_THREADS;++i){
@@ -92,10 +87,10 @@ int main(){
         pthread_join(threads[i], NULL);
     }
 
-    if (sum_array()!=NUM_THREADS*INNER_ITERATIONS*OUTER_ITERATIONS){
-        fprintf(stderr, "fgl: FAILED\n");   
+    if (sum_array()!=NUM_THREADS*ITERATIONS){
+        fprintf(stderr, "fgl: FAILED, actual: %d, expected: %d \n", sum_array(), NUM_THREADS*ITERATIONS);   
     }
     else{
-        fprintf(stderr, "fgl: SUCCEEDED\n");   
+        fprintf(stderr, "fgl: SUCCEEDED, expected: %d\n", NUM_THREADS*ITERATIONS);   
     }
 }
