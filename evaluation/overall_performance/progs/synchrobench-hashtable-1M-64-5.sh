7450e38b6fd52fc88cb32868da44e1953c402675
Tim Merrifield <timothym@vmware.com>
2017-11-26 22:57:15 -0600

    setting transaction setting to 1

--- evaluation/overall_performance/progs/synchrobench-hashtable-1M-64-5.sh
+++ evaluation/overall_performance/progs/synchrobench-hashtable-1M-64-5.sh
@@ -11,5 +11,5 @@
 
 cd $SYNCHROBENCH_PATH/c-cpp/bin/;
 ls;
-./MUTEX-hashtable -i $((1<<20)) -r $((1<<21)) -l 64 -t $1 -u 5 -d $((1000*60)) -x 0
+./MUTEX-hashtable -i $((1<<20)) -r $((1<<21)) -l 64 -t $1 -u 5 -d $((1000*60)) -x 1
 cd -;
