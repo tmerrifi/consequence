91dfa3bf103ad0b8132494a3120c3018788e716d
Tim Merrifield <timothym@vmware.com>
2017-11-26 22:59:57 -0600

    synchrobench driver script.

--- evaluation/overall_performance/configs/synchbench_ht.sh
+++ evaluation/overall_performance/configs/synchbench_ht.sh
@@ -1,12 +1,15 @@
-progs="synchrobench-hashtable-1M-8-1" #synchrobench-hashtable-1M-8-10 synchrobench-hashtable-1M-8-20"
+#progs="synchrobench-hashtable-1M-16-5 synchrobench-hashtable-1M-64-5 synchrobench-hashtable-1M-128-5"
 
-variants="CONVCOUNTERS=true CONVLOGGING=true mode=IC tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICLogging|gcc-consequence;\
-CONVCOUNTERS=true mode=ICNoCoarse tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqIC|gcc-consequence;\
-CONVCOUNTERS=true mode=ICSpec tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICSpec|gcc-consequence;\
-CONVCOUNTERS=true mode=ICWeak tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICWeak|gcc-consequence;\
-CONVCOUNTERS=true mode=ICWeakNoDet tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICWeak|gcc-consequence;\
-pthreads|pthreads|gcc-pthreads"
+progs="synchrobench-hashtable-1M-16-5"
+variants="CONVCOUNTERS=true mode=ICSpec tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICSpec|gcc-consequence"
 
-threads="8"
+#variants="CONVCOUNTERS=true mode=ICNoCoarse tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqIC|gcc-consequence;\
+#CONVCOUNTERS=true mode=ICSpec tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICSpec|gcc-consequence;\
+#CONVCOUNTERS=true mode=ICWeak tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICWeak|gcc-consequence;\
+#CONVCOUNTERS=true mode=ICWeakNoDet tokenclockadd=5000 minperiod=1000 maxperiod=200000|conseqICWeakNoDet|gcc-consequence;\
+#pthreads|pthreads|gcc-pthreads"
+
+threads="1 8 16 32"
 trials="1"
-ops_pattern="awk '{if (\$1==\"#txs\"){print \$3}}'"
+ops_pattern="cat \$rawOutputFile | awk '{if (\$1==\"#txs\"){print \$3}}'"
+parseConversionStats="true"
