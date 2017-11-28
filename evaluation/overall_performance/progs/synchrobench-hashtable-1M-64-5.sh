cc49eec7e67f91b5012f431aaea161f18343e57b
Tim Merrifield <timothym@vmware.com>
2017-11-28 15:52:09 -0600

    changing time

--- evaluation/overall_performance/progs/synchrobench-hashtable-1M-64-5.sh
+++ evaluation/overall_performance/progs/synchrobench-hashtable-1M-64-5.sh
@@ -11,5 +11,5 @@
 
 cd $SYNCHROBENCH_PATH/c-cpp/bin/;
 ls;
-./MUTEX-hashtable -i $((1<<20)) -r $((1<<21)) -l 64 -t $1 -u 5 -d $((1000*60)) -x 1
+./MUTEX-hashtable -i $((1<<20)) -r $((1<<21)) -l 64 -t $1 -u 5 -d $((1000*30)) -x 1
 cd -;
